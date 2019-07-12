1c8381c3b66a135de44f3d3c068cd19abf90c5f5
azzu018 <52479872+azzu018@users.noreply.github.com>
2019-07-12 11:58:31 +0530

    Create split.py

---
+++ split.py
@@ -0,0 +1,14 @@
+m=input()
+m=m.split()
+m1=m[0]
+m2=m[1]
+count=0
+i=0
+while(i<len(m1) and count<2):
+    if(m1[i]!=m2[i]):
+        count+=1
+    i+=1
+if(count==1 or count==0):
+    print("yes")
+else:
+    print("no")
